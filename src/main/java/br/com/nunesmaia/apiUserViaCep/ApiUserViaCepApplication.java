7f02f20b4679bac25a90659a066298e9d86e407c
Fabiano Nunesmaia <fanunesmaia@gmail.com>
2023-03-06 18:13:11 -0300

    criação do projeto

---
+++ src/main/java/br/com/nunesmaia/apiUserViaCep/ApiUserViaCepApplication.java
@@ -0,0 +1,13 @@
+package br.com.nunesmaia.apiUserViaCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserViaCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserViaCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nunesmaia/apiUserViaCep/ApiUserViaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nunesmaia.apiUserViaCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserViaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

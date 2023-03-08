afd9852798ca3096bb9834abceaf711183848448
Fabiano Nunesmaia <fanunesmaia@gmail.com>
2023-03-08 13:54:43 -0300

    Criação do controller.

---
+++ src/main/java/br/com/nunesmaia/apiUserViaCep/controller/UserController.java
@@ -0,0 +1,32 @@
+package br.com.nunesmaia.apiUserViaCep.controller;
+
+import br.com.nunesmaia.apiUserViaCep.model.User;
+import br.com.nunesmaia.apiUserViaCep.model.dto.UserDTO;
+import br.com.nunesmaia.apiUserViaCep.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService user;
+
+    @PostMapping("register")
+    public void register(@RequestBody User newUser){
+        this.user.reg(newUser);
+    }
+
+   // @PostMapping("/login")
+   // public User login(@RequestBody UserDTO userdto){
+
+    //}
+
+
+
+
+}

e1ecd756e9e2c712c4fc27cb3ccc0cab70288ca8
Fabiano Nunesmaia <fanunesmaia@gmail.com>
2023-03-08 11:30:22 -0300

    criação do UserDTO.

---
+++ src/main/java/br/com/nunesmaia/apiUserViaCep/model/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package br.com.nunesmaia.apiUserViaCep.model.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UserDTO {
+
+    private String email;
+    private String password;
+}

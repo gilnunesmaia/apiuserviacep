56d2a3358f9d6d970c8ee48abb0f5b204989b480
Fabiano Nunesmaia <fanunesmaia@gmail.com>
2023-03-08 17:21:35 -0300

    criação do método de validação do email e da senha.

--- src/main/java/br/com/nunesmaia/apiUserViaCep/service/UserService.java
+++ src/main/java/br/com/nunesmaia/apiUserViaCep/service/UserService.java
@@ -1,5 +1,6 @@
 package br.com.nunesmaia.apiUserViaCep.service;
 
+import br.com.nunesmaia.apiUserViaCep.exception.InvalidDataException;
 import br.com.nunesmaia.apiUserViaCep.model.User;
 import org.springframework.stereotype.Service;
 
@@ -8,27 +9,24 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
 
-        public void reg(User u) {
-            if (u.getName() ==null || u.getName().trim().isEmpty()){
-                System.out.println("invalid name");
-            }
-
-            if (u.getEmail() ==null || u.getEmail().trim().isEmpty()){
-                System.out.println("invalid email");
-            }
-
-            if (u.getPassword() ==null || u.getPassword().trim().isEmpty()){
-                System.out.println("invalid password");
-            }
-
-            if (u.getPostalCode() ==null || u.getPostalCode().trim().isEmpty()){
-                System.out.println("invalid password");
-            }
+    public void reg(User u) throws InvalidDataException {
+        this.validateEAP(u.getEmail(), u.getPassword());
+        if (u.getName() == null || u.getName().trim().isEmpty()){
+            throw new InvalidDataException("Nome vazio");
         }
 
 
 
+    }
 
+    private void validateEAP(String email, String password) throws InvalidDataException {
+        if (email == null || email.trim().isEmpty()) {
+            throw new InvalidDataException("email vazio");
+        }
+        if (password == null || password.trim().isEmpty()) {
+            throw new InvalidDataException("senha vazia");
+        }
+    }
 
 
 }
